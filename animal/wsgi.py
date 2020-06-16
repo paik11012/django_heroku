4af8cc454f831d49d7cb46b031e12d228ad5d7ec
paik11012 <minjnu11012@gmail.com>
2020-06-16 18:58:27 +0900

    initial commit

---
+++ animal/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for animal project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "animal.settings")
+ 
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+# push
+application = Cling(get_wsgi_application())
+  
